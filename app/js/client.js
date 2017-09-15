d3e039d564b8309db854432c201a62c92af009c7
Hayden Haddock <Hayden Haddock>
2017-09-15 13:56:05 -0500

    finishing touches

--- app/js/client.js
+++ app/js/client.js
@@ -1,3 +1,42 @@
-console.log("Working");
 var socket = io();
-console.log(socket);
+var user;
+
+$(document).ready(function(){
+  //Get user name and make sure it isnt null
+  user = prompt("Please enter your user name", "User");
+  if(user == null){
+    location.reload();
+  } else {
+    socket.emit('newUser', user);
+  }
+
+  //ease of use tool for submitting msg on enter key
+  $("#chatText").keypress(function(e){
+    //keycode for enter key
+    if(e.which == 13){
+      sendMsg();
+    }
+  })
+});
+
+function sendMsg(){
+  var msg = $("#chatText").val();
+  console.log(user+": " + msg);
+  socket.emit('sendMessage', user, msg);
+  $("#chatText").val("");
+}
+
+socket.on('allUsers', function(data){
+  for(i = 0; i < data.length; i++){
+    console.log(data[i]);
+    $("#connectedUserList").append("<li><h3><u>"+data[i].username+"</u></h3></li>");
+  }
+});
+socket.on('newUser', function(user){
+  console.log(user);
+  $("#connectedUserList").append("<li><h3><u>"+user.username+"</u></h3></li>");
+  $("#chatList").append("<li>User <strong><u>"+user.username+"</u></strong> has entered </li>")
+});
+socket.on('sendMessage', function(user, msg){
+  $("#chatList").append("<li><span><strong><u>"+user+"</u></strong><span>: <span>"+msg+"</span></li>")
+});
--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static('app'));
 
-app.get('/', function(req, res){
+app.get('/exp1', function(req, res){
  res.sendFile(__dirname + '/app/index.html');
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
  console.log("Listening on port 3000");
 });
 
+server.lastUserID = 0; //used to keep track of users
+
 io.on('connection', function(socket){
- console.log("a user has connected!");
+  console.log("a user has connected!");
+
+  socket.on('newUser', function(user){
+    socket.user = {
+      id: server.lastUserID++,
+      username: user
+    };
+    //Get all users in the room for new user
+    socket.emit('allUsers', getConnectedClients());
+    //Give all other users the new user
+    socket.broadcast.emit('newUser', socket.user);
+  });
+
+  socket.on('sendMessage', function(user, msg){
+    io.emit('sendMessage', user, msg);
+  });
 });
+
+function getConnectedClients(){
+  var users = [];
+  Object.keys(io.sockets.sockets).forEach(function(id) {
+    var user = io.sockets.connected[id].user;
+    if(user) users.push(user);
+  });
+  console.log(users);
+  return users;
+}

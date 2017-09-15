06bbbea8d84fb169bb2cec3dc2bd2ce2b4bf64e6
Hayden Haddock <Hayden Haddock>
2017-09-15 15:11:29 -0500

    finishing touches

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http');
-//var server = require('http').Server(app);
+var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/app'));

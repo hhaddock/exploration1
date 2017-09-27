8d042d5e04a21b9cbc700c3fd8a5cf3dcf079990
hhaddock <haydenhaddock1@gmail.com>
2017-09-27 15:21:19 -0500

    password auth

--- app/js/connection.js
+++ app/js/connection.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'D132263c@',
-  database: 'tcic'
+  password: 'Tigersblvd16',
+  database: 'chatApp'
 });
 
 connection.connect(function(err) {
   if (err) throw err
   console.log('Database Connection Successful!');
 });
-connection.query('USE tcic');
+connection.query('USE users');
 
 module.exports = connection;

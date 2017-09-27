a60bf9fc2d1ae85de355e22b0ee5de90959d44b3
Hayden Haddock <Hayden Haddock>
2017-09-27 16:20:51 -0500

    theme test

--- server.js
+++ server.js
@@ -25,11 +25,12 @@ io.on('connection', function(socket){
   socket.on('checkUser', function(user){
     var sql = 'SELECT * FROM users WHERE username = ?';
     db.query(sql, [user], function(err, res, fields){
-     if(res[0].username == user[1]){
-       socket.emit('checkUser', res[0].username);
-     } else {
-       socket.emit('checkUser', "Not Authorized");
-     }
+    //  if(res[0].username == user[1]){
+    //    socket.emit('checkUser', res[0].username);
+    //  } else {
+    //    socket.emit('checkUser', "Not Authorized");
+    //  }
+       socket.emit('checkUser', res);
     });
   });
 

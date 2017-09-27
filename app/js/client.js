079eb681c5b647b1fa4066372d69d819fb5e88e8
Hayden Haddock <Hayden Haddock>
2017-09-27 18:39:47 -0500

    theme test

--- app/js/client.js
+++ app/js/client.js
@@ -60,7 +60,7 @@ socket.on('sendMessage', function(user, msg){
   window.setInterval(function() {
     var elem = document.getElementById('chatList');
     elem.scrollTop = elem.scrollHeight;
-  }, 5000);
+  }, 2500);
 });
 
 socket.on('disconnect', function(user){

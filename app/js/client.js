9b7ec9f346fc09cde0fef08e95679ee5e019d253
Hayden Haddock <Hayden Haddock>
2017-10-28 13:20:00 -0500

    chat logs

--- app/js/client.js
+++ app/js/client.js
@@ -76,7 +76,23 @@ socket.on('getChatLogs', function(res){
   for(i = 0; i < res.length; i++){
     console.log(res[i]);
     $('.modal-body').html(
-      res[i].user + ": " + res[i].message
+      '<table style="width:100%">
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>'
     );
   }
 });

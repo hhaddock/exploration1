859dfe4aefaca502c37082f9c7b25f600a82ee08
Hayden Haddock <Hayden Haddock>
2017-10-28 12:52:29 -0500

    chat logs

--- server.js
+++ server.js
@@ -49,15 +49,12 @@ io.on('connection', function(socket){
   });
 
   socket.on('sendMessage', function(user, msg){
-    console.log(user)
-    // var sql = 'INSERT INTO chatLogs (user, message) VALUES ?';
-    // var values = [
-    //   [user, msg]
-    // ]
-    // db.query(sql, [values] , function(err, res, fields){
-    //   if (err) return console.log(err);
-    //   console.log(res);
-    // });
+    var sql = 'INSERT INTO chatLogs (user, message) VALUES ?';
+    var values = [user, msg]
+    db.query(sql, [values] , function(err, res, fields){
+      if (err) return console.log(err);
+      console.log(res);
+    });
     io.emit('sendMessage', user, msg);
   });
 
